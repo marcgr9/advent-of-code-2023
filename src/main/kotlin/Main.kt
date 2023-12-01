79cf43ac94b8c35b7e3275e4f5e066865ae86438
Marc Gruita <marc@gruita.ro>
2023-12-02 00:55:05 +0200

    day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import dayone.partOne
+import dayone.partTwo
+
+fun getResource(name: String) = object {}.javaClass.getResourceAsStream(name)!!.bufferedReader()
+
 fun main(args: Array<String>) {
     println("Hello, Advent of Code 2023!")
+    println("Day 1, part 1: ${partOne()}")
+    println("Day 1, part 2: ${partTwo()}")
 }
